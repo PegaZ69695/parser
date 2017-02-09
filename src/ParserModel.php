1e75ddcda497f41ea9f8acd39cab47a1dd427c04
PegaZ69695 <PegaZ69695@gmail.com>
2017-02-09 12:53:02 +0300

    fix pagination

--- src/ParserModel.php
+++ src/ParserModel.php
@@ -102,9 +102,9 @@ abstract class ParserModel
      * */
     public function getPagination($limit = null)
     {
-        $this->thisStatus = 1;
+        $this->thisStatus = 0;
         if ($this->items = $this->provider->find(static::SEARCH_STRING, 1, self::STATUS_ACTIVE, $limit)) {
-            $this->thisStatus = 0;
+            $this->thisStatus = 1;
             return $this;
         }
         
