931aa959f1359ead18cc90ab9003e997fcd56283
PegaZ69695 <PegaZ69695@gmail.com>
2017-06-26 14:24:26 +0300

    changed interface

--- src/DataProvider/DataProviderInterface.php
+++ src/DataProvider/DataProviderInterface.php
@@ -8,7 +8,6 @@
 
 namespace Parser\DataProvider;
 
-use Parser\Product\ProductModel;
 use Parser\ParserBase;
 
 interface DataProviderInterface
@@ -19,8 +18,8 @@ interface DataProviderInterface
     public function update($id, $status = ParserBase::STATUS_INACTIVE);
     public function find($like_, $type_, $active = 0, $limit = null);
     public function findProduct($findField, $findValue);
-    public function insertProductToDB(ProductModel $product);
-    public function updateProductToDB(ProductModel $product);
+    public function insertProductToDB($product);
+    public function updateProductToDB($product);
     public function hasCategoryParent($category_id, $parent_id);
     public function cloneDonorMenu($menuItems = [], $parentId);
     public function disableOldProduct($vendorId, $days);
